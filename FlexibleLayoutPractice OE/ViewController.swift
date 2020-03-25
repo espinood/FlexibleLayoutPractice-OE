af08ab75717eccc1ef85895dc7bd4e9a29d50a34
Omar Espinosa <espinood@rose-hulman.edu>
2020-03-25 10:46:23 -0400

    Initial Commit

---
+++ FlexibleLayoutPractice OE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlexibleLayoutPractice OE
+//
+//  Created by CSSE Department on 3/25/20.
+//  Copyright © 2020 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
